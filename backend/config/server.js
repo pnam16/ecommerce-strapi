f203e4a9d3e9855f8bc3029e0dd40545e13ca0a8
pnam <phuongnamhn2000@gmail.com>
2021-07-19 14:08:57 +0700

    init

--- backend/config/server.js
+++ backend/config/server.js
@@ -6,4 +6,15 @@ module.exports = ({ env }) => ({
       secret: env('ADMIN_JWT_SECRET', 'c28df8d21f2b2ffda41e983edc175254'),
     },
   },
+  email: {
+    provider: 'Strapi',
+    providerOptions: {
+      apiKey: env('SENDGRID_API_KEY'),
+    },
+    settings: {
+      defaultFrom: 'juliasedefdjian@strapi.io',
+      defaultReplyTo: 'juliasedefdjian@strapi.io',
+      testAddress: 'juliasedefdjian@strapi.io',
+    },
+  },
 });
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e58a45eb-e915-41d0-baeb-69da464641cd'
+};
