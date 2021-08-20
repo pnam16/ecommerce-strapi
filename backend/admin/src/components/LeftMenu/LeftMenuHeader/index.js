1f2f596aa2f25e4e901133cd8d6d21b5ee811c30
PNam <phuongnamhn2000@gmail.com>
2021-08-20 22:07:45 +0700

    update layout

---
+++ backend/admin/src/components/LeftMenu/LeftMenuHeader/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Wrapper from "./Wrapper";
+
+const LeftMenuHeader = () => (
+  <Wrapper>
+    <Link to="/" className="leftMenuHeaderLink">
+      <span className="projectName" >
+        Home
+      </span>
+    </Link>
+  </Wrapper>
+);
+
+export default LeftMenuHeader;
