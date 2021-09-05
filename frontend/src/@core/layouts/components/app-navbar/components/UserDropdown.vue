535902d07d6f92128d0c639c884ccc66c6935051
DUCANHHOANG\ADMIN <hoangducanh241049@gmail.com>
2021-09-05 11:58:02 +0700

    update

--- frontend/src/@core/layouts/components/app-navbar/components/UserDropdown.vue
+++ frontend/src/@core/layouts/components/app-navbar/components/UserDropdown.vue
@@ -7,9 +7,9 @@
     <template #button-content>
       <div class="d-sm-flex d-none user-nav">
         <p class="user-name font-weight-bolder mb-0">
-          {{ userData.fullName || userData.username }}
+          Nhóm 19
         </p>
-        <span class="user-status">{{ userData.role }}</span>
+        <span class="user-status"></span>
       </div>
       <b-avatar
         size="40"
